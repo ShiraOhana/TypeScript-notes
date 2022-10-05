fd235004032f4470b37b9fdda5b104b47f18c738
ShiraOhana <soshiraohana@gmail.com>
2022-10-05 17:58:34 +0300

    interface of the notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { StringLiteral } from "typescript";
 import "./App.css";
+import { Note } from "./models/note.models";
 
 function App() {
+  // Note[] structure (interface) is in "models" folder
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: new Date().toString(),
+      title: "Meetings",
+      text: "Schedual meeting with the team",
+      color: "#dfdfdf",
+      date: new Date().toDateString(),
+    },
+  ]);
+
   return <div className="App">App</div>;
 }
 
