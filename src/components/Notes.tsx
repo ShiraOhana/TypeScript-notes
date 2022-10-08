932843a80ad0ccffbf8180ed4b5e61b3a4aa8026
ShiraOhana <soshiraohana@gmail.com>
2022-10-08 18:55:16 +0300

    Passing props

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,1 +1,12 @@
-export {}
+import * as React from "react";
+import { Note } from "../models/note.models";
+
+interface INotesProps {
+  note: Note;
+}
+
+const Notes: React.FC<INotesProps> = ({ note }) => {
+  return <div> Note Card</div>;
+};
+
+export default Notes;
--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,15 +1,23 @@
 import * as React from "react";
 import { Note } from "../models/note.models";
+import Notes from "../components/Notes";
 
 // Type of props for the TS -> Created on models
 interface INotesListProps {
   notes: Note[];
 }
 
-const NotesList: React.FunctionComponent<INotesListProps> = (props) => {
+const NotesList: React.FunctionComponent<INotesListProps> = ({ notes }) => {
+  const renderNotes = (): JSX.Element[] => {
+    return notes.map((note) => {
+      return <Notes key={note.id} note={note} />;
+    });
+  };
+
   return (
     <>
       <h2 className="mt-3">Notes</h2>
+      <div>{renderNotes()}</div>
     </>
   );
 };
