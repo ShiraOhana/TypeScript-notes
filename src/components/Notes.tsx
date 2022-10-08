bd16c25db0bdc3c2d892842b2b55dedaae552b54
ShiraOhana <soshiraohana@gmail.com>
2022-10-08 19:24:37 +0300

    Deleting notes functunality

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -5,9 +5,10 @@ import { Note } from "../models/note.models";
 
 interface INotesProps {
   note: Note;
+  handleDelete: (id: string) => void;
 }
 
-const Notes: React.FC<INotesProps> = ({ note }) => {
+const Notes: React.FC<INotesProps> = ({ note, handleDelete }) => {
   return (
     <div className="mb-3">
       <Card style={{ backgroundColor: note.color }}>
@@ -15,7 +16,11 @@ const Notes: React.FC<INotesProps> = ({ note }) => {
           <Card.Title> {note.title}</Card.Title>
           <Card.Text> {note.text} </Card.Text>
           <Card.Subtitle className="text-muted"> {note.date} </Card.Subtitle>
-          <Button className="mt-3" variant="danger">
+          <Button
+            className="mt-3"
+            variant="danger"
+            onClick={() => handleDelete(note.id)}
+          >
             Delete
           </Button>
         </Card.Body>
--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { Note } from "../models/note.models";
 import Notes from "../components/Notes";
+import { setSyntheticLeadingComments } from "typescript";
 
 // Type of props for the TS -> Created on models
 interface INotesListProps {
   notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
 }
 
-const NotesList: React.FunctionComponent<INotesListProps> = ({ notes }) => {
+const NotesList: React.FunctionComponent<INotesListProps> = ({
+  notes,
+  setNotes,
+}) => {
+  const handleDelete = (id: string) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
   const renderNotes = (): JSX.Element[] => {
     return notes.map((note) => {
-      return <Notes key={note.id} note={note} />;
+      return <Notes key={note.id} note={note} handleDelete={handleDelete} />;
     });
   };
 
